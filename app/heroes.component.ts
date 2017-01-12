6eae855d10d2a52d1315554dfa99cd9e82e25705
Joseph Blasingame <blasingame.joseph@gmail.com>
2017-01-11 19:25:42 -0600

    Adding Current Work in Prog

---
+++ app/heroes.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-heroes',
+  templateUrl: 'heroes.component.html',
+  styleUrls: [ 'heroes.component.css' ],
+  
+  providers: [HeroService]
+})
+export class HeroesComponent implements OnInit { 
+  
+  name = 'Joseph!';
+  heroes: Hero[];  
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+}
